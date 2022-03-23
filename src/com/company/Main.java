59aea6b7bed632c3c62997308a9da32c57b9be1b
Maksim Grishunin <maksgris@gmail.com>
2022-03-23 10:12:57 +0200

    Solution of task 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,16 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
+        // First exercise
+
+        int arr[][] = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                arr[i][j] = (i+1) * (j+1);
+                String str = String.valueOf(arr[i][j]);
+                System.out.print(String.format("%4s",str));
+            }
+            System.out.println("\n");
+        }
     }
 }
